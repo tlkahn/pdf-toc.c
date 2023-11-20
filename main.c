8dbbd415388a2fcafa8e8aca61d5b35815a1c332
tlkahn <joshgy@gmail.com>
2023-11-20 16:06:20 +0800

    Refactored extract_toc function, added parse_outline for better structure
    Major changes:
    
    1. Added cJSON *parse_outline(fz_context *ctx, fz_outline *outline) function.
    
    2. Moved TOC parsing logic from extract_toc to parse_outline.
    
    3. Simplified extract_toc by calling parse_outline and handling JSON operations

--- main.c
+++ main.c
@@ -2,41 +2,56 @@
 #include <cjson/cJSON.h>
 #include <stdio.h>
 
+cJSON *parse_outline(fz_context *ctx, fz_outline *outline)
+{
+    cJSON *toc_array = cJSON_CreateArray();
+
+    while (outline)
+    {
+        cJSON *toc_item = cJSON_CreateObject();
+        cJSON_AddStringToObject(toc_item, "title", outline->title);
+        cJSON_AddNumberToObject(toc_item, "page", outline->page.page + 1);
+
+        if (outline->down)
+        {
+            cJSON *subitems = parse_outline(ctx, outline->down);
+            cJSON_AddItemToObject(toc_item, "subitem", subitems);
+        }
+
+        cJSON_AddItemToArray(toc_array, toc_item);
+        outline = outline->next;
+    }
+
+    return toc_array;
+}
+
 char *extract_toc(const char *filepath)
 {
     fz_context *ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
     fz_register_document_handlers(ctx);
 
-    cJSON *toc_array = cJSON_CreateArray();
     char *json_string = NULL;
 
     fz_try(ctx)
     {
-
         fz_document *doc = fz_open_document(ctx, filepath);
         fz_outline *outline = fz_load_outline(ctx, doc);
-        while (outline)
-        {
-            cJSON *toc_item = cJSON_CreateObject();
-            cJSON_AddStringToObject(toc_item, "title", outline->title);
-            cJSON_AddNumberToObject(toc_item, "page", outline->page.page + 1);
-            cJSON_AddItemToArray(toc_array, toc_item);
-            outline = outline->next;
-        }
+        cJSON *toc_array = parse_outline(ctx, outline);
 
         fz_drop_outline(ctx, outline);
         fz_drop_document(ctx, doc);
         fz_drop_context(ctx);
+
+        json_string = cJSON_PrintUnformatted(toc_array);
+        cJSON_Delete(toc_array);
     }
     fz_catch(ctx)
     {
         fprintf(stderr, "Cannot open document: %s\n", filepath);
-        cJSON_Delete(toc_array);
         fz_drop_context(ctx);
         return NULL;
     }
-    json_string = cJSON_PrintUnformatted(toc_array);
-    cJSON_Delete(toc_array);
+
     return json_string;
 }
 
